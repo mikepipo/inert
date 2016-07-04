34d19d36417ce405357c19ea8f8a29b563aa9a56
Alice Boxhall <aboxhall@chromium.org>
2016-07-04 11:39:58 -0700

    WIP

--- inert.js
+++ inert.js
@@ -15,107 +15,232 @@
  * limitations under the License.
  */
 
-class InertWrapper {
-  constructor(root) {
-    if (!root) {
-      throw new Error('Missing required argument; InertWrapper needs to wrap an Element.');
-    }
-    this._root = root;
-    this._savedNodes = [];
+class InertRoot {
+  constructor(rootElement, inertManager) {
+    this._inertManager = inertManager;
+    this._rootElement = rootElement;
+    this._rootElement.setAttribute('aria-hidden', 'true');
+    this._savedNodes = new Set([]);
 
-    this._inert = true;
-    makeInert();
+    this._walkSubtree(this._rootElement);
+
+    // mutation observer
+    this._observer = new MutationObserver(this._onChildAdded);
+    this._observer.observe(this._rootElement, { childList: true, subtree: true });
+  }
+
+  destructor() {
+    this._observer.disconnect();
+    delete this._observer;
+
+    if (this._rootElement && this._rootElement.parentNode)
+      this._rootElement.removeAttribute('aria-hidden');
+    delete this._rootElement;
+
+    for (let inertNode of this._savedNodes) {
+      inertNode.removeInertRoot(this);
+      this._savedNodes.delete(inertNode);
+    }
+    delete this._savedNodes;
+
+    delete this._inertManager;
+  }
+
+  /**
+   * @param {Node} startNode
+   */
+  _walkSubtree(startNode) {
+    InertManager.composedTreeWalk(startNode, { preorder: (node) => { return this._visitNode(node); } });
+  }
+
+  /**
+   * @param {Node} node
+   */
+  _visitNode(node) {
+    if (!node.matches(InertNode._focusableElementsString) && !node.hasAttribute('tabindex'))
+      return;
+
+    let inertNode = this._inertManager.register(node, this);
+    this._savedNodes.add(inertNode);
+  }
+
+  _onChildAdded(records, self) {
+    for (let record of records) {
+      for (let node of Array.from(record.addedNodes))
+        this._walkSubtree(node);
+    }
+  }
+}
+
+class InertNode {
+  /**
+   * @param {Node} node
+   * @param {InertRoot} inertRoot
+   */
+  constructor(node, inertRoot) {
+    this._node = node;
+    this._node.__inertNode = this;
+
+    this._inertRoots = new Set([inertRoot]);
+    this._destroyed = false;
+
+    if (node.matches(InertNode._focusableElementsString)) {
+      if (node.hasAttribute('tabindex'))
+        this._savedTabIndex = node.tabIndex;
+      this._node.setAttribute('tabindex', '-1');
+    } else if (node.hasAttribute('tabindex')) {
+      this._savedTabIndex = node.tabIndex;
+      this._node.removeAttribute('tabindex');
+    }
+  }
+
+  destructor() {
+    this._throwIfDestroyed();
+
+    if (this._node.parentNode) {
+      if ('_savedTabIndex' in this)
+        this._node.setAttribute('tabindex', this.savedTabIndex);
+      else
+        this._node.removeAttribute('tabindex');
+    }
+    delete this._node.__inertNode;
+    delete this._node;
+    delete this._inertRoots;
+
+    this._destroyed = true;
   }
 
   static get _focusableElementsString() {
     return 'a[href],area[href],input:not([disabled]),select:not([disabled]),textarea:not([disabled]),button:not([disabled]),iframe,object,embed,[contenteditable]';
   }
 
-  get inert() {
-    return this._inert;
+  get destroyed() {
+    return this._destroyed;
   }
 
-  makeNodeInert(node) {
-    // Skip subtrees which already have 'inert' set
-    if (node.hasAttribute('inert'))
-      return false;
-
-    var nodeData = { node: node };
-    if (node.matches(InertWrapper._focusableElementsString)) {
-      if (node.hasAttribute('tabindex'))
-        nodeData.savedTabIndex = node.tabIndex;
-      node.setAttribute('tabindex', '-1');
-      this._savedNodes.push({node: node, });
-    } else if (node.hasAttribute('tabindex')) {
-      nodeData.savedTabIndex = node.tabIndex;
-      node.removeAttribute('tabindex');
-    }
-    this._savedNodes.push(nodeData);
-
-    return true;
+  _throwIfDestroyed() {
+    if (this.destroyed)
+      throw new Error("Trying to access destroyed InertNode");
   }
 
-  
-  makeInert(root) {
-    InertWrapper.composedTreeWalk(root, { preorder: (node) => { makeNodeInert(node) } });
-
-    // add mutation observer
+  get node() {
+    this._throwIfDestroyed();
+    return this._node;
   }
 
-  makeNodeErt(nodeData) {
-    var node = nodeData.node;
-    if ('savedTabIndex' in nodeData)
-      node.setAttribute('tabindex', savedTabIndex);
-    else
-      node.removeAttribute('tabindex');
-    return true;
+  set savedTabIndex(tabIndex) {
+    this._throwIfDestroyed();
+    this._savedTabIndex = tabIndex;
   }
 
-  makeErt() {
-    while (this._savedNodes.length > 0) {
-      var nodeData = this._savedNodes.shift();
-      makeNodeErt(nodeData);
-    }
+  get savedTabIndex() {
+    this._throwIfDestroyed();
+    return this._savedTabIndex;
   }
 
-  set inert(isInert) {
-    if (this._inert === isInert) {
-      return;
-    }
+  addInertRoot(inertRoot) {
+    this._throwIfDestroyed();
+    this._inertRoots.add(inertRoot);
+  }
 
-    this._inert = isInert;
-    if (isInert)
-      makeInert();
-    else
-      makeNonInert();
-    });
+  removeInertRoot(inertRoot) {
+    this._throwIfDestroyed();
+    this._inertRoots.delete(inertRoot);
+    if (this._inertRoots.size === 0)
+      this.destructor();
+  }
+}
+
+class InertManager {
+  /**
+   * @param {Document} document
+   */
+  constructor(document) {
+    if (!document)
+      throw new Error('Missing required argument; InertManager needs to wrap a document.');
+
+    this._document = document;
+    this._savedNodes = new Set([]);
+    this._inertRoots = new Set([]);
+
+    var inertElements = Array.from(document.querySelectorAll('[inert]'));
+    for (let inertElement of Array.from(inertElements))
+      this.setInert(inertElement, true);
   }
 
   /**
-   * Recursively walk the composed tree from |node|, aborting if |end| is encountered.
+   * @param {Element} root
+   * @param {boolean} inert
+   */
+  setInert(root, inert) {
+    if (inert) {
+      if (root.__inertRoot)  // Check for inert attribute?
+        return;
+
+      let inertRoot = new InertRoot(root, this);
+      root.__inertRoot = inertRoot;
+      root.setAttribute('inert', '');
+      this._inertRoots.add(inertRoot);
+    } else {
+      if (!root.__inertRoot)  // Check for inert attribute?
+        return;
+
+      let inertRoot = root.__inertRoot;
+      inertRoot.destructor();
+      this._inertRoots.delete(inertRoot);
+      delete root.__inertRoot;
+      root.removeAttribute('inert');
+    }
+  }
+
+  /**
+   * @param {Node} node
+   * @param {InertRoot} inertRoot
+   * @return {InertNode} inertNode
+   */
+  register(node, inertRoot) {
+    let inertNode = undefined;
+    // TODO better way to associate nodeData with node?
+    if (node.__inertNode) {
+      inertNode = node.__inertNode;
+      inertNode.inertRoots.add(inertRoot);
+    } else {
+      inertNode = new InertNode(node, inertRoot);
+    }
+    this._savedNodes.add(inertNode);
+
+    return inertNode;
+  }
+
+  /**
+   * @param {InertNode} inertNode
+   * @param {InertRoot} inertRoot
+   */
+  deregister(inertNode, inertRoot) {
+    inertNode.removeInertRoot(inertRoot);
+    if (inertNode.destroyed)
+      this._savedNodes.delete(inertNode);
+  }
+
+  /**
+   * Recursively walk the composed tree from |node|.
    * @param {Node} node
    * @param {{preorder: (function (Node):boolean|undefined),
    *          postorder: (function (Node)|undefined)}} callbacks
-   *     Callbacks to be called for each element traversed, excluding
-   *     |end|. Possible callbacks are |preorder|, called before descending into
-   *     child nodes, and |postorder| called after all child nodes have been
-   *     traversed. If |preorder| returns false, its child nodes will not be
-   *     traversed.
+   *     Callbacks to be called for each element traversed. Possible
+   *     callbacks are |preorder|, called before descending into child
+   *     nodes, and |postorder| called after all child nodes have been
+   *     traversed. If |preorder| returns true, its child nodes will
+   *     not be traversed.
    * @param {ShadowRoot=} opt_shadowRoot The nearest ShadowRoot ancestor, if any.
-   * @return {boolean} Whether |end| was found, if provided.
    */
-  static function composedTreeWalk(node, callbacks, opt_shadowRoot) {
-    if (node === end)
-      return true;
-
+  static composedTreeWalk(node, callbacks, opt_shadowRoot) {
     if (node.nodeType == Node.ELEMENT_NODE)
       var element = /** @type {Element} */ (node);
 
-    var found = false;
-
     if (element && callbacks.preorder) {
-      if (!callbacks.preorder(element))
-        return found;
+      if (callbacks.preorder(element))
+        return;
     }
 
     // Descend into node:
@@ -125,13 +250,12 @@ class InertWrapper {
     if (element) {
       var shadowRoot = element.shadowRoot || element.webkitShadowRoot;
       if (shadowRoot) {
-        found = axs.dom.composedTreeSearch(shadowRoot,
-                                           end,
-                                           callbacks,
-                                           shadowRoot);
-        if (element && callbacks.postorder && !found)
+        InertManager.composedTreeWalk(shadowRoot,
+                                      callbacks,
+                                      shadowRoot);
+        if (element && callbacks.postorder)
           callbacks.postorder(element);
-        return found;
+        return;
       }
     }
 
@@ -141,60 +265,47 @@ class InertWrapper {
     if (element && element.localName == 'content') {
       var content = /** @type {HTMLContentElement} */ (element);
       var distributedNodes = content.getDistributedNodes();
-      for (var i = 0; i < distributedNodes.length && !found; i++) {
-        found = axs.dom.composedTreeSearch(distributedNodes[i],
-                                           end,
-                                           callbacks,
-                                           opt_shadowRoot);
+      for (var i = 0; i < distributedNodes.length; i++) {
+        InertManager.composedTreeWalk(distributedNodes[i],
+                                              callbacks,
+                                              opt_shadowRoot);
       }
-      if (element && callbacks.postorder && !found)
+      if (element && callbacks.postorder)
         callbacks.postorder.call(null, element);
-      return found;
+      return;
     }
 
     // If it is neither the parent of a ShadowRoot, a <content> element, nor
     // a <shadow> element recurse normally.
     var child = node.firstChild;
-    while (child != null && !found) {
-      found = axs.dom.composedTreeSearch(child,
-                                         end,
-                                         callbacks,
-                                         opt_shadowRoot);
+    while (child != null) {
+      InertManager.composedTreeWalk(child,
+                                    callbacks,
+                                    opt_shadowRoot);
       child = child.nextSibling;
     }
 
-    if (element && callbacks.postorder && !found)
+    if (element && callbacks.postorder)
       callbacks.postorder.call(null, element);
-    return found;
   }
 
 }
 
-function observe(records, self) {
+document.inertManager = new InertManager(document);
+
+function watchForInert(records, self) {
   for (var record of records) {
     if (record.type != 'attributes')
       continue;
     if (record.attributeName != 'inert')
       continue;
     var target = record.target;
-    if (target.detabinator === undefined)
-        target.detabinator = new Detabinator(target);
     var inert = target.hasAttribute('inert');
-    target.detabinator.inert = inert;
-    if (inert)
-      target.setAttribute('aria-hidden', 'true');
-    else
-      target.removeAttribute('aria-hidden')
+    document.inertManager.setInert(target, inert);
   }
 }
 
-var observer = new MutationObserver(observe);
-observer.observe(document.body, { attributes: true, subtree: true });
+// Uncomment to allow watching for inert attribute changes
+// var observer = new MutationObserver(watchForInert);
+// observer.observe(document.body, { attributes: true, subtree: true });
 
-var inertElements = document.querySelectorAll('[inert]');
-for (var i = 0; i < inertElements.length; i++) {
-  var inertElement = inertElements[i];
-  inertElement.detabinator = new Detabinator(inertElement);
-  inertElement.detabinator.inert = true;
-  inertElement.setAttribute('aria-hidden', 'true');
-}
